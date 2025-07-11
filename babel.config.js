43d864c4216d2e3b767e273237f7d5d543fe6f34
Pavel Potoček <pavelpotocek@gmail.com>
2025-07-11 17:41:10 +0200

    victory-native

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,8 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      'react-native-reanimated/plugin', // 👈🏼 add this as the LAST item in plugins
+    ],
   };
 };
