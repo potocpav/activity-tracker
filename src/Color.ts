49eac4c9a86732cfc6072dc81ffe9bc4bfc45089
Pavel Potoček <pavelpotocek@gmail.com>
2025-07-21 13:35:00 +0200

    palette file

---
+++ src/Color.ts
@@ -0,0 +1,45 @@
+export const lightPalette = [
+  '#D32F2F', // red_700
+  '#F4511E', // deep_orange_700
+  '#F57C00', // orange_700
+  '#FF8F00', // amber_800
+  '#F9A825', // yellow_800
+  '#AFB42B', // lime_700
+  '#7CB342', // light_green_600
+  '#388E3C', // green_700
+  '#00897B', // teal_600
+  '#00ACC1', // cyan_600
+  '#039BE5', // light_blue_600
+  '#1976D2', // blue_700
+  '#303F9F', // indigo_700
+  '#5E35B1', // deep_purple_600
+  '#8E24AA', // purple_600
+  '#D81B60', // pink_600
+  '#5D4037', // brown_700
+  '#757575', // grey_600
+  '#424242', // grey_800
+  '#000000', // black
+];
+
+export const darkPalette = [
+  '#EF9A9A', // red_200
+  '#FFAB91', // deep_orange_200
+  '#FFCC80', // orange_200
+  '#FFECB3', // amber_100
+  '#FFF59D', // yellow_200
+  '#E6EE9C', // lime_200
+  '#C5E1A5', // light_green_200
+  '#69F0AE', // green_A200
+  '#80CBC4', // teal_200
+  '#80DEEA', // cyan_200
+  '#81D4FA', // light_blue_200
+  '#64B5F6', // blue_300
+  '#9FA8DA', // indigo_200
+  '#B39DDB', // deep_purple_200
+  '#CE93D8', // purple_200
+  '#F48FB1', // pink_200
+  '#BCAAA4', // brown_200
+  '#AAAAAA', // grey_300
+  '#DDDDDD', // grey_100
+  '#FFFFFF', // white
+];
