678fc8656d81b1e8e3377308b30426c43b9def8a
Pavel Potoček <pavelpotocek@gmail.com>
2025-09-28 13:11:23 +0200

    Fix ticks for weird units

--- src/Components/FlatListChart.tsx
+++ src/Components/FlatListChart.tsx
@@ -76,10 +76,11 @@ export const barBoundingBox = (value: number | null): BoundingBox => {
 }
 
 const cmpMajorTicks = (unit: SubUnit, range: { min: number, max: number }, approxNTicks: number): number[] => {
-  const numberStride = () => {
-    const idealStride = (range.max - range.min) / approxNTicks;
+  const idealStride = (range.max - range.min) / approxNTicks;
+
+  const defaultStride = (targetStride: number) => {
     const logBase = Math.pow(10, 1/3); 
-    const logStride = Math.round(Math.log(idealStride) / Math.log(logBase));
+    const logStride = Math.round(Math.log(targetStride) / Math.log(logBase));
     let fractionalStride = 0;
     switch ((Math.round(logStride) % 3 + 3) % 3) {
       case 0:
@@ -96,6 +97,18 @@ const cmpMajorTicks = (unit: SubUnit, range: { min: number, max: number }, appro
     return stride;
   }
 
+  const enumeratedStride = (targetStride: number, strideList: number[]) => {
+    let [min, minStride] = [Infinity, Infinity];
+    for (const stride of strideList) {
+      const diff = Math.abs(Math.log(stride) - Math.log(targetStride));
+      if (diff < min) {
+        min = diff;
+        minStride = stride;
+      }
+    }
+    return minStride;
+  }
+
   const stridedTicks = (stride: number) => {
     let ticks = [];
     for (let i = Math.ceil(range.min / stride); i <= Math.floor(range.max / stride); i++) {
@@ -104,22 +117,59 @@ const cmpMajorTicks = (unit: SubUnit, range: { min: number, max: number }, appro
     return ticks;
   }
 
-  let stride = Infinity;
+  let stride = defaultStride(idealStride);
 
   switch (unit.type) {
     case "count":
-      stride = numberStride();
+      stride = defaultStride(Math.max(1, idealStride));
       break;
     case "number":
     case "distance":
     case "weight":
-      stride = numberStride();
+      stride = defaultStride(idealStride);
       break;
-    
     case "time":
-      stride = numberStride();
+      switch (unit.unit) {
+        case "hours":
+          if (idealStride < 24) {
+            stride = enumeratedStride(idealStride, [1/3600, 2/3600, 5/3600, 10/3600, 30/3600, 1/60, 2/60, 5/60, 10/60, 15/60, 30/60, 1, 2, 6, 12, 24]);
+          }
+          break;
+        case "seconds":
+          if (idealStride >= 1 && idealStride < 3600 * 24) {
+            stride = enumeratedStride(idealStride, [2, 5, 10, 30, 1*60, 2*60, 5*60, 10*60, 15*60, 30*60, 1*3600, 2*3600, 6*3600, 12*3600, 24*3600]);
+          }
+          break;
+      }
       break;
     case "climbing_grade":
+      switch (unit.grade) {
+        case "french":
+          if (idealStride < 10) {
+            stride = enumeratedStride(idealStride, [1/6, 1/3, 1, 2, 5]);
+          }
+          break;
+        case "uiaa":
+          if (idealStride < 10) {
+            stride = enumeratedStride(idealStride, [1/6, 1/3, 1, 2, 5]);
+          }
+          break;
+        case "yds":
+          if (idealStride < 10) {
+            stride = enumeratedStride(idealStride, [1/4, 1, 2, 5]);
+          }
+          break;
+        case "font":
+          if (idealStride < 10) {
+            stride = enumeratedStride(idealStride, [1/6, 1/3, 1, 2, 5]);
+          }
+          break;
+        case "v-scale":
+          if (idealStride < 10) {
+            stride = enumeratedStride(idealStride, [1/2, 1, 2, 5]);
+          }
+          break;
+      }
       break;  
   }
   return stridedTicks(stride);
